a0e7e0d1d92753b32227cf59bad5333afd491b1d
seiji.ishibe@gmail.com <seiji.ishibe@gmail.com@b1d7264d-0df9-25d5-604d-6dc64666845a>
2012-09-06 13:50:50 +0000

    testeCCLient.sh

---
+++ trunk/scripts/testeCClient.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 	
+
+basepath=doc/programsOut/
+pathDocClient=${basepath}clients/
+ipConnectionServerLevel2={"0" "localhost" "localhost" "localhost" "localhots"}
+binfolder=bin/
+
+
+portServer2=("0" "51003" "51003" "51004" "51004" "51005" "51005" "51006" "51006")
+
+for i in `seq 1 4`; do
+	echo Cliente $i
+	clientC $(binfolder)Cliente ${ipConnectionServerLevel2[$i]} ${portServer2[$i]} >> ${pathDocClient}clientJava$i.txt &
+done
+
