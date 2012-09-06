da89a68713f83df5cb63b68404dd60493c12d008
seiji.ishibe@gmail.com <seiji.ishibe@gmail.com@b1d7264d-0df9-25d5-604d-6dc64666845a>
2012-09-06 13:52:52 +0000

    testeCClient Corrigido

--- trunk/scripts/testeCClient.sh
+++ trunk/scripts/testeCClient.sh
@@ -10,6 +10,6 @@ portServer2=("0" "51003" "51003" "51004" "51004" "51005" "51005" "51006" "51006"
 
 for i in `seq 1 4`; do
 	echo Cliente $i
-	clientC $(binfolder)Cliente ${ipConnectionServerLevel2[$i]} ${portServer2[$i]} >> ${pathDocClient}clientJava$i.txt &
+	clientC $(binfolder)Cliente ${ipConnectionServerLevel2[$i]} ${portServer2[$i]} >> ${pathDocClient}clientC$i.txt &
 done
 
