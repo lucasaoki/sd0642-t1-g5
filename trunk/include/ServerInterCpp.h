f96cbebb76849ff2b1109dceaedd72b459d183bb
aoki.ec.usp@gmail.com <aoki.ec.usp@gmail.com@b1d7264d-0df9-25d5-604d-6dc64666845a>
2012-09-09 22:17:48 +0000

    buscando bug

--- trunk/include/ServerInterCpp.h
+++ trunk/include/ServerInterCpp.h
@@ -1,23 +1,9 @@
-/**
-    @mainpage Doxygen My Test
-    @brief How can I use doxygen documentation
-    
-    @index 
-    The documentation became beautifull
- */
-
 #ifndef SERVCLIENT_H
 #define SERVCLIENT_H
 
 #include "ServerCpp.h"
 #include "ClientCpp.h"
 
-/** 
-        @class Client Client.h "include/Client.h"
-        @brief A class for teste doxygen file
-
-        A class to test draw inheritance        
- */
 class ControlClass {
 public:
     
@@ -26,12 +12,6 @@ public:
 class ServerInterCpp : public ServerCpp, ClientCpp {
 public:
 
-    /**
-    -------------------------------------------------------------
-        client puro,    ./x ipServer porta
-        servclient,     ./x portaDele nivel ipServer portaServer
-    -------------------------------------------------------------
-     */
     ServerInterCpp(char* argv[]) : ServerCpp(argv[1]), ClientCpp(argv[3], argv[4]) {
         setArraySize(atoi(argv[2]));
         isFinalServer_ = 0;
